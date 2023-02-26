d7c6d0cf28e252c5844cc215d4f5761d398e3b39
Pirated <71497650+ItsYash27@users.noreply.github.com>
2023-02-26 07:46:41 +0530

    Update check_alive.py

--- plugins/check_alive.py
+++ plugins/check_alive.py
@@ -6,7 +6,7 @@ CMD = ["/", "."]
 
 @Client.on_message(filters.command("alive", CMD))
 async def check_alive(_, message):
-    await message.reply_text("ɪᴍ ᴀʟɪᴠᴇ ɪ sᴛɪʟʟ ʜᴀᴠ 69ʏʀs ᴛᴏ ʟɪᴠᴇ :) 𝖧𝗂𝗍 /start \n\n𝖧𝗂𝗍 /help 𝖥𝗈𝗋 𝖧𝖾𝗅𝗉 ;)\n\n\n𝖧𝗂𝗍 /ping 𝖳𝗈 𝖢𝗁𝖾𝖼𝗄 𝖡𝗈𝗍 𝖯𝗂𝗇𝗀 😁")
+    await message.reply_text("ɪᴍ ᴀʟɪᴠᴇ\n ɪ sᴛɪʟʟ ʜᴀᴠ 69ʏʀs ᴛᴏ ʟɪᴠᴇ 😈")
 
 @Client.on_message(filters.command("ping", CMD))
 async def ping(_, message):
@@ -14,4 +14,4 @@ async def ping(_, message):
     rm = await message.reply_text("...........")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"𝖯𝗂𝗇𝗀!\n{time_taken_s:.3f} ms")
+    await rm.edit(f"ᴘɪɴɢ!\n{time_taken_s:.3f} ms")
